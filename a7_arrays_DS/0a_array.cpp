895a8b922c31e02dc3b956db2b0d1a655544134d
Shikhar Dutta <149324583+unknownunicorn01@users.noreply.github.com>
2026-08-03 23:00:15 +0530

    Enhance array.cpp with memory storage comments
    Added comments explaining array memory storage and indexing.

--- a7_arrays_DS/0a_array.cpp
+++ a7_arrays_DS/0a_array.cpp
@@ -1,1 +1,20 @@
-
+#include<iostream>
+using namespace std;
+int main(){
+  int arr[] = {1,2,3,4,5};
+// How array is stored in memory in memory
+  /*
+      //lets say that address of array start at memory location 401
+memory location:  401  402  403  404  405 
+                  ____ ____ ____ ____ ____
+                 | 1  | 2  | 3  | 4  |  5 |
+                  ____ ____ ____ ____ ____
+    Indexes :      0    1    2   3    4
+  */
+  for(int i=0;i<sizeof(arr)/sizeof(int);i++){
+    cout<<arr[i]<<" ";
+  }
+  cout<<endl;
+  cout<<"Code executed";
+  return 0;
+}
